78e712066e17b1ec90b401cb974f7d439ef66e36
mitesh-200228 <81156492+mitesh-200228@users.noreply.github.com>
2022-01-03 18:10:52 +0530

    x

--- src/components/Addons/Info.js
+++ src/components/Addons/Info.js
@@ -1,13 +1,74 @@
 import React from 'react';
 import './addons.css'
 
-function Info (props){
-return (
-	<div className="pizzasize">
-        <button> {props.size} ₹ {props.price}</button>
-        {/* <span className="sizeprice"> ₹ {props.price}</span> */}
-    </div>
-
-)
+function Info(props) {
+    const [x, setX] = React.useState(0);
+    const Hello = () => {
+        setX(1);
+    }
+    return (
+        <div className="pizzasize">
+            <button className='btn111' onClick={Hello}>
+                {props.size}
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                ₹ {props.price}
+            </button>
+            {/* <span className="sizeprice"> </span> */}
+        </div>
+    )
 }
 export default Info;
