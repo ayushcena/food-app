06b33e5b8ae78119accee12ad3e89f53cd5531f6
mitesh-200228 <81156492+mitesh-200228@users.noreply.github.com>
2022-03-28 19:43:02 +0530

    done

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -31,25 +31,29 @@ export const slice = createSlice({
         RemoveItemToCart: (state, action) => {
             let truth = 0;
             let si = 0;
-            if(action.payload.topping){
+            if (action.payload.topping) {
                 for (let i = 0; i < state.cartItems.length; i++) {
                     if (state.cartItems[i].productId === action.payload.productId) {
                         si = i;
-                        if(state.cartItems[i].topping.length === action.payload.topping.length){
-                            for (let j = 0; j < state.cartItems[i].topping.length; j++){
-                                if(state.cartItems[i].topping[j] === action.payload.topping[j]){
+                        if (state.cartItems[i].topping.length === action.payload.topping.length) {
+                            for (let j = 0; j < state.cartItems[i].topping.length; j++) {
+                                if (state.cartItems[i].topping[j] === action.payload.topping[j]) {
                                     truth++;
                                 }
                             }
-                            if(truth === state.cartItems[i].topping.length && truth === action.payload.topping.length){
-                                state.cartItems[si].quantity -= 1;
-                                state.cartItems[si].totalPrice -= action.payload.price;                                   
+                            if (truth === state.cartItems[i].topping.length && truth === action.payload.topping.length) {
+                                if(state.cartItems[si].quantity >= 0 && state.cartItems[si].totalPrice >= 0){
+                                    state.cartItems[si].quantity -= 1;
+                                    state.cartItems[si].totalPrice -= action.payload.price;
+                                    console.log(state.cartItems[si].quantity);
+                                    console.log(state.cartItems[si].totalPrice);
+                                }
                             }
                         }
                     }
                 }
             }
-            else{
+            else {
                 for (let i = 0; i < state.cartItems.length; i++) {
                     if (state.cartItems[i].productId === action.payload.productId) {
                         state.cartItems[i].quantity -= 1;
@@ -61,25 +65,25 @@ export const slice = createSlice({
         }, addItemToCartSpecified: (state, action) => {
             let truth = 0;
             let si = 0;
-            if(action.payload.topping){
+            if (action.payload.topping) {
                 for (let i = 0; i < state.cartItems.length; i++) {
                     if (state.cartItems[i].productId === action.payload.productId) {
                         si = i;
-                        if(state.cartItems[i].topping.length === action.payload.topping.length){
-                            for (let j = 0; j < state.cartItems[i].topping.length; j++){
-                                if(state.cartItems[i].topping[j] === action.payload.topping[j]){
+                        if (state.cartItems[i].topping.length === action.payload.topping.length) {
+                            for (let j = 0; j < state.cartItems[i].topping.length; j++) {
+                                if (state.cartItems[i].topping[j] === action.payload.topping[j]) {
                                     truth++;
                                 }
                             }
-                            if(truth === state.cartItems[i].topping.length && truth === action.payload.topping.length){
+                            if (truth === state.cartItems[i].topping.length && truth === action.payload.topping.length) {
                                 state.cartItems[si].quantity += 1;
-                                state.cartItems[si].totalPrice += action.payload.price;                                   
+                                state.cartItems[si].totalPrice += action.payload.price;
                             }
                         }
                     }
                 }
             }
-            else{
+            else {
                 for (let i = 0; i < state.cartItems.length; i++) {
                     if (state.cartItems[i].productId === action.payload.productId) {
                         state.cartItems[i].quantity += 1;
@@ -116,28 +120,29 @@ export const slice = createSlice({
             }
             var mo = false;
             var index = 0;
-            for (let j = 0; j < state.cartItems.length; j++) {
-                for (var i = 0; i < action.payload.addons.length; i++) {
-                    if (action.payload.addons[i].drink === state.cartItems[j].name) {
+            var idx = 0;
+            console.log(action.payload);
+            for (let j = 0; j < action.payload.addons.length ; j++) {
+                for (var i = 0; i < state.cartItems.length; i++) {
+                    if (action.payload.addons[j].drink === state.cartItems[i].name) {
                         mo = true;
-                        index = j;
-                        break;
+                        index = i;
+                        state.cartItems[index].totalPrice += state.cartItems[index].price;
+                        state.cartItems[index].quantity += 1;
                     }
                 }
-            }
-            if (!mo) {
-                for (let i = 0; i < action.payload.addons.length; i++) {
+                if (!mo) {
+                    // for (let i = 0; i < action.payload.addons.length; i++) {
                     state.cartItems.push({
-                        name: action.payload.addons[i].drink,
-                        price: action.payload.addons[i].price,
-                        quantity: action.payload.addons[i].quantity,
-                        totalPrice: action.payload.addons[i].price * action.payload.addons[i].quantity,
-                        productId: action.payload.addons[i].productId
+                        name: action.payload.addons[j].drink,
+                        price: action.payload.addons[j].price,
+                        quantity: action.payload.addons[j].quantity,
+                        totalPrice: action.payload.addons[j].price * action.payload.addons[j].quantity,
+                        productId: action.payload.addons[j].productId
                     })
+                    // }
                 }
-            } else {
-                state.cartItems[index].totalPrice += state.cartItems[index].price;
-                state.cartItems[index].quantity += 1;
+                mo = false;
             }
         }
     }
